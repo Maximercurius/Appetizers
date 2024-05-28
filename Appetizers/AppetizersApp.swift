9511ce130104b3aa003f80fb60e5add20846d419
Maxim <swiftdevfromsiberia@gmail.com>
2024-05-28 21:40:38 +0500

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Makarov_Maxim on 28.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
