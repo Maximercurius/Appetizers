953633234a094d57ef6af321f7bd5f9798179193
Maxim <swiftdevfromsiberia@gmail.com>
2024-06-02 20:59:38 +0500

    Data model xccreated with mockData

---
+++ Appetizers/Appetizer.swift
@@ -0,0 +1,29 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by Makarov_Maxim on 02.06.2024.
+//
+
+import Foundation
+
+struct Appetizer: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+struct MockData{
+    static let sampleAppetizer = Appetizer(id: 0001, name: "Teast Appetizer", description: "Descriptoion", price: 7.77, imageURL: "", calories: 777, protein: 77, carbs: 77)
+    
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}
