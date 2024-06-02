4835e0dd13c7421eac2f9cd12201ef683028a1d6
Maxim <swiftdevfromsiberia@gmail.com>
2024-06-03 00:42:57 +0500

    Appetizer List created

--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -10,8 +10,12 @@ import SwiftUI
 struct AppetizerListView: View {
     var body: some View {
         NavigationView {
-            Text("Appetizer List View")
-                .navigationTitle("🥙 Appetizers")
+            List(MockData.appetizers) { appetizer in
+                AppetizerListCell(appetizer: appetizer)
+
+            }
+            .navigationTitle("🍟 Appetizers")
+            
         }
     }
 }
