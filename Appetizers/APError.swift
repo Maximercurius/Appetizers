db635adb599dac3d9eac0e6a727dfcdaa48384e8
Maxim <swiftdevfromsiberia@gmail.com>
2024-06-03 22:07:06 +0500

    NetworkManager created

---
+++ Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Makarov_Maxim on 03.06.2024.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
