6031107520fd2dd8c9efa4e6722567f146c1a072
Maxim <swiftdevfromsiberia@gmail.com>
2024-06-07 22:12:05 +0500

    Loading view with indicator created

---
+++ Appetizers/Color+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Ext.swift
+//  Appetizers
+//
+//  Created by Makarov_Maxim on 07.06.2024.
+//
+
+import SwiftUI
+
+extension Color {
+    static let branPrimary = Color("brandPrimary")
+}
+
+extension UIColor {
+    static let branPrimary = UIColor(named: "brandPrimary")
+}
