fc1962e832ff435cf44e4070356dfccc71ef1d64
Maxim <swiftdevfromsiberia@gmail.com>
2024-07-01 11:30:15 +0500

    Detail view refactoring

---
+++ Appetizers/Views/Buttons/XDismissButton.swift
@@ -0,0 +1,27 @@
+//
+//  XDismissButton.swift
+//  Appetizers
+//
+//  Created by Makarov_Maxim on 01.07.2024.
+//
+
+import SwiftUI
+
+struct XDismissButton: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .frame(width: 30, height: 30)
+                .foregroundColor(.white)
+                .opacity(0.7)
+            Image(systemName: "xmark")
+                .imageScale(.small)
+                .frame(width: 44, height: 44)
+                .foregroundColor(.black)
+        }
+    }
+}
+
+#Preview {
+    XDismissButton()
+}
