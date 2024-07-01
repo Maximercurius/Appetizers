9845d23bf1f953ab00b40df9cfe91a14195f61b1
Maxim <swiftdevfromsiberia@gmail.com>
2024-07-01 23:12:35 +0500

    Personal Info screen is created and tuned

--- Appetizers/Screens/AccouuntView/AccountView.swift
+++ Appetizers/Screens/AccouuntView/AccountView.swift
@@ -8,10 +8,39 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthDay = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+
+    
     var body: some View {
         NavigationView {
-            Text("Account")
-                .navigationTitle("👤 Account")
+            Form {
+                Section(header: Text("Personal Info")) {
+                    TextField("First name", text:$firstName)
+                    TextField("Last name", text:$lastName)
+                    TextField("E-mail", text:$email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                    DatePicker("Birth Day", selection: $birthDay, displayedComponents: .date)
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text ("Save Changes")
+                    }
+                }
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Freuent refills", isOn: $frequentRefills)
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+            }
+            
+            .navigationTitle("👤 Account")
         }
     }
 }
